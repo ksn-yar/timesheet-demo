eae82bdc98d88523aeb24695b87b1364e4ce6121
Yaroslav Koshman <ksn.yar@gmail.com>
2026-03-21 20:46:46 +0300

    remove unused autocreated dirs change load configs in kernel

--- app/src/Kernel.php
+++ app/src/Kernel.php
@@ -5,9 +5,23 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    private function configureContainer(ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder): void
+    {
+        $configDir = $this->getConfigDir();
+
+        $container->import($configDir.'/{packages}/*.{php,yaml}');
+        $container->import($configDir.'/{packages}/'.$this->environment.'/*.{php,yaml}');
+
+        $container->import($configDir.'/services.{php,yaml}');
+        $container->import($configDir.'/services_'.$this->environment.'.{php,yaml}');
+    }
 }
