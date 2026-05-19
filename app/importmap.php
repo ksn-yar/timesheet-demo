00c6c3e6ac619c16f74877aa00a721848037e647
Yaroslav Koshman <ksn.yar@gmail.com>
2026-05-19 19:06:38 +0300

    * add bootstrap

--- app/importmap.php
+++ app/importmap.php
@@ -16,4 +16,14 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
 ];
